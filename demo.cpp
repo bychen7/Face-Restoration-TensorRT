d19dce9cd90af75b4c6015eef4e474b7f08129d7
lsch <lukasschaupp@gmail.com>
2023-09-08 09:50:58 +0200

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -12,28 +12,6 @@
 
 namespace py = pybind11;
 
-// Forward declaration for the FaceRestore class
-class FaceRestore;
-
-PYBIND11_MODULE(python_face_restore, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 face restoration
-    )pbdoc";
-    
-    // Define the FaceRestore class and its methods
-    py::class_<FaceRestore>(m, "FaceRestore")
-        .def(py::init<>())
-        .def("setup", &FaceRestore::setup)
-        .def("inference", &FaceRestore::inference);
-
-#ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-    m.attr("__version__") = "dev";
-#endif
-}
-
-// Define the FaceRestore class
 class FaceRestore {
 public:
     FaceRestoration sample;  // Declare a FaceRestoration object
@@ -57,14 +35,20 @@ public:
     }
 };
 
-// Other functions (call_go, FaceRestoration, etc.) should be defined outside the FaceRestore class.
+PYBIND11_MODULE(python_face_restore, m) {
+    m.doc() = R"pbdoc(
+        Pybind11 face restoration
+    )pbdoc";
+    
+    // Define the FaceRestore class and its methods
+    py::class_<FaceRestore>(m, "FaceRestore")
+        .def(py::init<>())
+        .def("setup", &FaceRestore::setup)
+        .def("inference", &FaceRestore::inference);
 
-// Example usage:
-// int main() {
-//     FaceRestore faceRestore;
-//     faceRestore.setup();
-//     cv::Mat inputImage = cv::imread("input_image.jpg");
-//     cv::Mat outputImage = faceRestore.inference(inputImage);
-//     cv::imwrite("output_image.jpg", outputImage);
-//     return 0;
-// }
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
