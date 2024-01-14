5adda29fd1944d169d0268d3bb6faeb6db554804
lsch <lukasschaupp@gmail.com>
2024-01-14 13:44:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ import timeit
 
 restore = FaceRestoration.FaceRestoration("models/model_256.engine")
 img = cv2.imread("images/test.png")
-img = cv2.resize(img, (256,256))
+img = cv2.resize(img, (512,512))
+batch_img = np.repeat(img, 8, axis=0)
 start = timeit.default_timer()
-out = restore.infer(img)
+outputs = restore.infer(batch_img)
 stop = timeit.default_timer()
 
 print('Time: ', stop - start)
-cv2.imwrite("final.png", out) 
-print(out.shape)
+for output in outputs:
+  cv2.imwrite("final.png", out) 
+  print(out.shape)
